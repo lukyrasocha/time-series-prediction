0723345c4ff307eb37749567d148242fc82ca0bf
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-04-25 23:05:31 +0200

    Change main.py to work with mlflow projects

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import pandas as pd
 import mlflow
-from pipeline import pipeline
-
+from utils.transformers import preprocessor
+from datetime import datetime
 from azureml.core import Workspace
-ws = Workspace.from_config()
-mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
+import sys
+import numpy as np
+import argparse
 
-## NOTE: Optionally, you can use the public tracking server.  Do not use it for data you cannot afford to lose. See note in assignment text. If you leave this line as a comment, mlflow will save the runs to your local filesystem.
+#ws = Workspace.from_config()
+#mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
 
-# mlflow.set_tracking_uri("http://training.itu.dk:5000/")
+#SETUP
 mlflow.set_experiment("lukr - Assignment3")
-#mlflow.sklearn.autolog(log_input_examples=True, silent=True)
+#model = sys.argv[2] if len(sys.argv) > 1 else 'lin_reg' 
+#if model not in ['knn','lin_reg']:
+#    raise "Error: Argument not known: default{knn,lin_reg}"
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-poly", "--polydegree",type=int)
+parser.add_argument("-model", "--modelname",type=str)
+parser.add_argument("-splits", "--number_of_splits",type=int)
+parser.add_argument("-n_neigh", "--n_neighbours", type=int)
+parser.add_argument("-w", "--weights", type=str)
+
+args = parser.parse_args()
+
+model = args.modelname
+if model not in ['knn','lin_reg']:
+    raise "Error: Argument not known: default{knn,lin_reg}"
 
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
@@ -21,26 +39,52 @@ from sklearn.model_selection import TimeSeriesSplit
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 # Start a run
-with mlflow.start_run(run_name="25.04.2022"):
+name = f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} {model}" 
+with mlflow.start_run(run_name=name):
 
     df = pd.read_json("data/dataset.json", orient="split")
-    df.dropna(inplace=True)
+    df = df.drop(['Source_time','Lead_hours','ANM','Non-ANM'],axis=1)
+    df.dropna(subset=['Total','Direction'], inplace=True) #Speed will be imputed in the pipeline
 
     # TODO: Handle missing data
 
- #   pipeline = Pipeline([
-        # TODO: You can start with your pipeline from assignment 1
-#    ])
+    mlflow.log_param('model_name', model)
+    if model == 'lin_reg':
+        poly_degree = args.polydegree#int(sys.argv[3])
+        number_of_splits = args.number_of_splits #int(sys.argv[4])
+        pipeline = Pipeline(steps = [
+                   ('preprocessor', preprocessor)
+                  ,('poly_features', PolynomialFeatures(degree=poly_degree, include_bias=False))
+                  ,('regressor',LinearRegression())
+               ])
+
+        mlflow.log_param("poly_degree", poly_degree)
+        mlflow.log_param("number_of_splits", number_of_splits)
+        
+
+    else:
+        n_neighbours= args.n_neighbours #int(sys.argv[3])
+        weights = args.weights #sys.argv[4]
+        number_of_splits = args.number_of_splits #int(sys.argv[5])
+        pipeline = Pipeline(steps = [
+           ('preprocessor', preprocessor)
+          ,('regressor',KNeighborsRegressor(n_neighbors=n_neighbours, weights=weights))
+        ])
+
+        mlflow.log_param('n_neighbours', n_neighbours)
+        mlflow.log_param('weights', weights)
+        mlflow.log_param("number_of_splits", number_of_splits)
 
     # TODO: Currently the only metric is MAE. You should add more. What other metrics could you use? Why?
     metrics = [
         ("MAE", mean_absolute_error, []),
+        ("MSE", mean_squared_error, []),
+        ("r2", r2_score, [])
     ]
 
     X = df[["Speed","Direction"]]
     y = df["Total"]
 
-    number_of_splits = 5
 
     #TODO: Log your parameters. What parameters are important to log?
     #HINT: You can get access to the transformers in your pipeline using `pipeline.steps`
@@ -65,4 +109,10 @@ with mlflow.start_run(run_name="25.04.2022"):
             # NOTE: Here we just log the mean of the scores. 
             # Are there other summarizations that could be interesting?
             mean_score = sum(scores)/number_of_splits
+            var_score = np.var(scores)
             mlflow.log_metric(f"mean_{name}", mean_score)
+            mlflow.log_metric(f"Variance_{name}", var_score)
+
+    mlflow.sklearn.log_model(pipeline, "model")
+
+
