456c8349b690571b3b415f454560fedec9ed78d5
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-04-25 19:21:39 +0200

    Renamed template to 'main.py'

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pandas as pd
 import mlflow
 from pipeline import pipeline
 
-## NOTE: You can use Microsoft Azure Machine Learning Studio for experiment tracking. Follow assignment description and uncomment below for that (you might also need to pip azureml (pip install azureml-core):
 from azureml.core import Workspace
 ws = Workspace.from_config()
 mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
@@ -10,11 +9,9 @@ mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
 ## NOTE: Optionally, you can use the public tracking server.  Do not use it for data you cannot afford to lose. See note in assignment text. If you leave this line as a comment, mlflow will save the runs to your local filesystem.
 
 # mlflow.set_tracking_uri("http://training.itu.dk:5000/")
+mlflow.set_experiment("lukr - Assignment3")
+#mlflow.sklearn.autolog(log_input_examples=True, silent=True)
 
-# TODO: Set the experiment name
-mlflow.set_experiment("lukr - test")
-
-# Import some of the sklearn modules you are likely to use.
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
@@ -24,10 +21,9 @@ from sklearn.model_selection import TimeSeriesSplit
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 # Start a run
-# TODO: Set a descriptive name. This is optional, but makes it easier to keep track of your runs.
-with mlflow.start_run(run_name="<descriptive name>"):
-    # TODO: Insert path to dataset
-    df = pd.read_json("dataset.json", orient="split")
+with mlflow.start_run(run_name="25.04.2022"):
+
+    df = pd.read_json("data/dataset.json", orient="split")
     df.dropna(inplace=True)
 
     # TODO: Handle missing data
@@ -54,10 +50,10 @@ with mlflow.start_run(run_name="<descriptive name>"):
         predictions = pipeline.predict(X.iloc[test])
         truth = y.iloc[test]
 
-        from matplotlib import pyplot as plt 
-        plt.plot(truth.index, truth.values, label="Truth")
-        plt.plot(truth.index, predictions, label="Predictions")
-        plt.show()
+ #       from matplotlib import pyplot as plt 
+ #       plt.plot(truth.index, truth.values, label="Truth")
+ #       plt.plot(truth.index, predictions, label="Predictions")
+ #       plt.show()
         
         # Calculate and save the metrics for this fold
         for name, func, scores in metrics:
