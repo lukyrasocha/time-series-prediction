0ad67290e84ecf8c9fc54c8d485443eb7ed16e87
Lukas Rasocha <rasocha.lukas@gmail.com>
2022-05-01 15:19:10 +0200

    Add a new figure for all the runs

--- visualize.py
+++ visualize.py
@@ -5,6 +5,13 @@ import matplotlib.pyplot as plt
 
 runs = pd.read_csv('runs.csv')
 
+runs['ids'] = runs.index
+
+runs = runs.loc[(runs.ids != 17) & (runs.ids !=18)]
+sns.barplot(x="ids", y="mean_r2", data=runs,hue='model_name')
+plt.xticks(rotation=90)
+plt.savefig('test.png')
+
 lin_reg = runs[runs['model_name']=='lin_reg']
 knn_reg = runs[runs['model_name']=='knn']
 
@@ -31,4 +38,6 @@ print(knn_reg[knn_reg['mean_r2'] == max(knn_reg['mean_r2'])])
 best = lin_reg.loc[(lin_reg['poly_degree'] == 4)]
 plt.figure(3)
 sns.barplot(x="number_of_splits", y="mean_r2", data=best,linewidth=2.5, facecolor=(1, 1, 1, 0),errcolor=".2", edgecolor=".2")
-plt.savefig('number_of_splits_best.png')
+#plt.savefig('number_of_splits_best.png')
+
+
